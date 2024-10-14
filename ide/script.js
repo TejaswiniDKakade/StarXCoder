e234fb441d8980e04c36ab208cf613357b3628d1
TejaswiniDKakade <vaishukakade.12@gmail.com>
2024-10-14 14:40:53 +0530

    adding files

--- ide/script.js
+++ ide/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var languageSelector = document.getElementById("language-selector");
 var codeEditor = document.getElementById("code-editor");
 var runButton = document.getElementById("run-button");
@@ -18,3 +19,25 @@ runButton.addEventListener("click", function() {
     output.innerHTML = output;
   }
 });
+=======
+var languageSelector = document.getElementById("language-selector");
+var codeEditor = document.getElementById("code-editor");
+var runButton = document.getElementById("run-button");
+var output = document.getElementById("output");
+
+runButton.addEventListener("click", function() {
+  var language = languageSelector.value;
+  var code = codeEditor.value;
+
+  // Compile and run the code.
+  var compiler = new compiler(language);
+  var result = compiler.compile(code);
+  if (result.errors) {
+    output.innerHTML = result.errors.join("<br>");
+  } else {
+    var interpreter = new interpreter(language);
+    var output = interpreter.interpret(result.output);
+    output.innerHTML = output;
+  }
+});
+>>>>>>> d3b1d15 (adding files)
